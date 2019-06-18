205a46830d0b5cc19c4cb76209dbeb6f3d9e1721
bj-narola <47177823+bj-narola@users.noreply.github.com>
2019-06-18 08:24:08 +0530

    Update notes_controller.rb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -17,7 +17,7 @@ class NotesController < ApplicationController
   def create
     note = current_user.notes.build(note_params)
     return false unless note.save
-
+    p "========================"
     msg = 'New Note Added'
     check_reminder(msg, note, params[:do_remind], params[:remind_date][0])
   end
